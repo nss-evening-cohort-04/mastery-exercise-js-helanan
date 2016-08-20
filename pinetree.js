46731126a0f12974ba298e496307f978d583bc2e
Helana Nosrat <helanan@gmail.com>
2016-08-20 13:41:29 -0500

    added and fixed alert/event listeners, added extra loop and character, fixed spaces and fixed missing brackets, completed

--- pinetree.js
+++ pinetree.js
@@ -2,18 +2,21 @@ function tree(height, character) {
   var growtree = [];
   growtree[height] = character;
 
-  for (var i = 0; i < growtree.length; i++) {
-   growtree[i];
+  for (var i = 0; i <= height; i++) {
+  growtree[i] = "" + " ";  
   }
+
+ growtree[height + 1] = character;
+ var treeStart = growtree.join(""); 
+ console.log(treeStart);
 
   for (var j = 0; j < height; j++) {
-    growtree[height - j] = character;
+  growtree[height - j] = character + character;
 
-    var treeOutput = growtree.join(" ");
-    console.log(treeOutput);
-  }
-};
-
+  var treeOutput = growtree.join("");
+  console.log(treeOutput);
+  };
+}
 var nameInput = document.getElementById("height");
 var btn = document.getElementById("btn");
 btn.addEventListener("click", validation);
@@ -21,13 +24,10 @@ btn.addEventListener("click", validation);
 function validation(){
 var heightinput = document.getElementById("height").value;
 var shapeinput = document.getElementById("shape").value;
-if (heightinput === "" && shapeinput === "") {
+if (heightinput === "" || shapeinput === "") {
   alert ("Need A Value");
-  } 
-  else {
-    tree (heightinput, shapeinput)
   }
-}
-
-
-  
+  else {
+  tree (heightinput, shapeinput)
+  }
+};
